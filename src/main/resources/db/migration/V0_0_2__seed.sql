e4d8915a3728c6675f9ad0ed321cc4cc428b9ec3
beep101 <rajko.p.97@gmail.com>
2021-02-25 01:38:33 +0100

    Flyway migrations added to project

---
+++ src/main/resources/db/migration/V0_0_2__seed.sql
@@ -0,0 +1,2 @@
+insert into users(email,passwd,name,surname) values('rajko@mail.com','$2a$10$P0YNbzrER/TVusB5Et3Oyerhs65BKFd2KPcBjK.hJF0/zZzMPYvuC','Rajko','Pavlovic');
+insert into users(email,passwd,name,surname) values('faruk@mail.com','$2a$10$hvwpPZdQjSlEjDJtbajiHuXPgmMBEHXPevF5UiYq6/.Syu7XqXJDC','Faruk','Ljuca');
