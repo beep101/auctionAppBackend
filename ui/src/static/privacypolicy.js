61ece8d751a7d8b9f776e8e3c499ba83b82f583a
beep101 <rajko.p.97@gmail.com>
2021-02-26 16:50:26 +0100

    Content added to static pages

--- ui/src/static/privacypolicy.js
+++ ui/src/static/privacypolicy.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function PrivacyPolicy(){
     return(
-        <div>Privacy Policy</div>
+        <div class="textContainer">
+            <h1 class="highlight">Privacy Policy</h1>
+            <p class="textBody">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque felis risus, convallis a volutpat non, interdum eget orci. In sed vulputate purus. Nulla sed lectus a mauris sollicitudin rhoncus. Suspendisse potenti. Suspendisse quis mauris in tellus commodo malesuada sit amet rutrum orci. Donec odio mi, semper vitae aliquam sed, scelerisque et ipsum. Ut pulvinar odio sit amet laoreet ornare. Donec quis laoreet dui. Vivamus sodales ultricies ligula ut vehicula. Integer pretium congue lorem. Suspendisse sapien enim, finibus rutrum tincidunt eu, tempor vitae erat. Cras justo sapien, scelerisque sit amet fringilla nec, dignissim vitae orci. Donec a ex eleifend, molestie augue at, pretium elit. Vestibulum eleifend, mi malesuada sodales lacinia, metus quam varius ligula, eu aliquet ligula lectus iaculis elit. Fusce consequat, turpis ac faucibus interdum, leo ex congue ex, nec fringilla est odio et magna. Nunc felis neque, semper vel lacus et, pretium semper tortor. Ut accumsan vitae enim in consectetur. Aliquam eget tincidunt neque. Curabitur vehicula faucibus ligula, ac fringilla velit ultrices vitae. In quis ante id ligula rutrum facilisis at at nisl. In sapien nunc, ultricies eu auctor vel, auctor non sapien. Nam euismod lectus eros. Sed bibendum arcu vel tortor bibendum, vel vulputate sapien pretium. 
+            </p>
+            <p class="textBody">
+                Suspendisse potenti. Aenean iaculis erat eget nisi volutpat finibus. Ut iaculis pellentesque tellus. Mauris cursus interdum enim, eu efficitur urna fermentum nec. In a metus sit amet ex euismod venenatis a at est. Nam lacinia lacinia euismod. Vestibulum id libero ac tellus volutpat sodales. Integer id odio pretium, mollis lacus at, commodo tortor. In venenatis elementum eros ut tempor. Pellentesque tempus libero tincidunt orci euismod, id malesuada velit lacinia. Vivamus in arcu at ipsum ullamcorper malesuada ultricies eu nulla. 
+            </p>
+            <p class="textBody">
+            Sed ac purus vel ligula tempus rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vitae mattis leo. Nullam scelerisque tellus ultricies massa auctor ultrices. Suspendisse fermentum blandit gravida. Etiam eu massa eget odio dignissim rhoncus vel tristique libero. In laoreet neque at mauris molestie porttitor. Sed a luctus est. Sed arcu dolor, mollis in erat a, faucibus vehicula dui. 
+            </p>
+        </div>
     )
 }
 
