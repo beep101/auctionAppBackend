f2fe639780f9dc13f37d4c93419611be7da0834b
beep101 <rajko.p.97@gmail.com>
2021-03-18 14:57:37 +0100

    Removed some annotations

--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -6,14 +6,9 @@ import javax.validation.constraints.Email;
 
 public class UserModel {
 	private int id;
-	@NotBlank(message = "First name can't be blank")
 	private String firstName;
-	@NotBlank(message = "Last name can't be blank")
 	private String lastName;
-	@Email(message = "Email address must be valid")
-	@NotBlank(message = "Email can't be blank")
 	private String email;
-	@Size(min = 6, message = "Password can't be shorter than 6 characters")
 	private String password;
 	private String jwt;
 	private String forgotPasswordToken;
